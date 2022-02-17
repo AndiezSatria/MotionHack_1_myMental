80e95ee4a37b5ea5ab39e464a195f19ee2f98bc7
AndiezSatria <andis.permana12@gmail.com>
2022-02-17 23:51:34 +0700

    Setup Landing Page

--- lib/ui/pages/landing_page.dart
+++ lib/ui/pages/landing_page.dart
@@ -6,6 +6,86 @@ class LandingPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/images/bg.png"),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SvgPicture.asset(
+                  "assets/svg/logo_leaf.svg",
+                  width: 90.0,
+                  height: 90.0,
+                ),
+                const SizedBox(height: 24),
+                Text(
+                  "myMental",
+                  style: Theme.of(context).textTheme.headline4?.copyWith(
+                        fontWeight: FontWeight.bold,
+                        color: secondaryColor,
+                      ),
+                ),
+              ],
+            ),
+          ),
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 48),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 160.0,
+                    decoration: BoxDecoration(
+                      borderRadius: const BorderRadius.all(
+                        Radius.circular(100),
+                      ),
+                      border: Border.all(color: Colors.transparent),
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Color(0x59ECECEC),
+                          blurRadius: 12.0,
+                          spreadRadius: 6.0,
+                        )
+                      ],
+                      color: mainColor,
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Center(
+                        child: InkWell(
+                          onTap: () {
+                            Get.toNamed(LoginPage.routeName);
+                          },
+                          child: Text(
+                            "Lanjutkan",
+                            style:
+                                Theme.of(context).textTheme.bodyText1?.copyWith(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
